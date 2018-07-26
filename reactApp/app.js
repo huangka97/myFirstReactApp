6af68f8133af3a15a8ba55cdea0a8abbf1cd0901
Karl Huang <karl.huang2009@gmail.com>
2018-07-26 15:45:13 -0700

    asdfsad

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>React lives!</p>,
+const dummyData=["Come to SF","Master React","Sleep"];
+
+class ToDoList extends React.Component{
+
+	constructor(props){
+		super(props)
+	}
+	render(){
+		return(
+			<ul>
+			{this.props.data.map((item)=> <li>{item}</li>)}
+			
+			</ul>
+
+		)
+	}
+}
+
+ReactDOM.render(<ToDoList data={dummyData}/>,
    document.getElementById('root'));
